6013b9cf87f61be14e8682ee8db5a8f7b32b2ff0
Accurate <accurate@Accurates-MacBook-Pro.local>
2018-09-05 22:05:46 -0300

    novos estudos realizados

---
+++ curso_IOS/SwiftQuiz/SwiftQuiz/QuizViewController.swift
@@ -0,0 +1,65 @@
+//
+//  QuizViewController.swift
+//  SwiftQuiz
+//
+//  Created by Accurate on 05/09/18.
+//  Copyright © 2018 Cassio. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    @IBOutlet weak var viTimer: UIView!
+    @IBOutlet weak var lbQuestion: UILabel!
+    @IBOutlet var btAnswers: [UIButton]!
+    
+    let quizManager = QuizManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        viTimer.frame.size.width = view.frame.size.width
+        UIView.animate(withDuration: 60.0, delay: 0, options: .curveLinear, animations: {
+            self.viTimer.frame.size.width = 0
+        }) { (sucess) in
+            self.showResults();
+        }
+        
+        getNewQuiz();
+    }
+    
+    func getNewQuiz(){
+        quizManager.refreshQuiz();
+        lbQuestion.text = quizManager.question
+        for i in 0..<quizManager.options.count{
+            let option = quizManager.options[i]
+            let button = btAnswers[i]
+            button.setTitle(option, for: .normal)
+        }
+    }
+    
+    func showResults(){
+        performSegue(withIdentifier: "resultSegue", sender: nil)
+    }
+    
+    @IBAction func selectAnswer(_ sender: UIButton) {
+        guard let index = btAnswers.index(of: sender) else { return }
+        quizManager.validateAnswer(index: index)
+        getNewQuiz()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController = segue.destination as! ResultViewController
+        resultViewController.totalAnswers = quizManager.totalAnswers
+        resultViewController.totalCorrectAnswers = quizManager.totalCorrectAnswers
+    }
+}
+
+
+
+
